2fb0d17170a21492ce81f57fe33bd5264aed7163
Anshuman Ranjan <500062237@stu.upes.ac.in>
2021-01-29 10:38:37 +0530

    Update spread_operator.js

--- spread_operator.js
+++ spread_operator.js
@@ -15,3 +15,28 @@ the variable with which it is passed.
 2. Here, '+' operation is made to spread across array variable 'arrval'
 inside the function sum.
 */
+
+
+
+/*
+            Spread operator vs Concat method.
+*/
+
+let arr1 = [1, 2, 3];
+let arr2 = [4, 5, 6];
+let arr3 = [7, 8, 9];
+
+//1st method.
+//arr1 = arr1.concat(arr2);
+
+//2nd method.
+arr1 = [...arr1, ...arr2, ...arr3];
+console.log(arr1);
+
+//Here, spread operator is utilized
+//which helps to concatenate as many
+//arrays as we want.
+
+
+
+
