fd259cadbfd1b72d8131bad02743f111e117d24a
Anshuman Ranjan <500062237@stu.upes.ac.in>
2021-01-29 13:51:03 +0530

    Update array_destructuring.js

--- array_destructuring.js
+++ array_destructuring.js
@@ -18,3 +18,15 @@ let [ele1, ele2, ele3] = myproglang;
 console.log("The first element is " + ele1);
 console.log("The second element is " + ele2);
 console.log("The third element is " + ele3);
+
+//To find the length of the array.
+console.log(`The length of the array is ${myproglang.length}`);
+
+
+//To skip the elements / to print the first and last elements.
+let [firstelement, , , , lastelement] = myproglang;
+console.log(`The first programming language is ${firstelement} and the last
+programming language is ${lastelement}`);
+
+
+
